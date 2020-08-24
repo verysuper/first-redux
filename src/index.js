94beb58dc6e2f6b47da442c41ec22a1220fb53ce
熊大 <brown@brown.com>
2020-08-24 22:42:17 +0800

    store.dispatch({type:"add"})

--- src/index.js
+++ src/index.js
@@ -4,6 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const { createStore } = require('redux');
+function counter(state = 0, action){
+  switch( action.type ){
+    case "add":
+      return state +1;
+    default:
+      return state;
+  }  
+}
+const store = createStore(counter)
+store.subscribe(function(){
+  console.log('有更新', store.getState())
+})
+store.dispatch({type:"add"})
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
