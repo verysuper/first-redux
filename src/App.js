fd38c2c3db1d3d6e063935e23ca93665458bc3bd
Ziv <ziv@idisplay.com.tw>
2020-08-27 18:14:47 +0800

    add router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { connect } from 'react-redux'
+import { connect, useDispatch } from 'react-redux'
 import {
     BrowserRouter as Router,
     Switch,
@@ -8,56 +8,73 @@ import {
     Link,
     useRouteMatch,
     useParams
-  } from "react-router-dom";
+} from "react-router-dom";
+import { useLocation} from "react-router";
 
-  function Home() {
+function Home() {
     return <h2>Home</h2>;
-  }
-  
-  function About() {
-    return <h2>About</h2>;
-  }
-  
-  function Topics() {
-    let match = useRouteMatch();
-  
-    return (
-      <div>
-        <h2>Topics</h2>
-  
-        <ul>
-          <li>
-            <Link to={`${match.url}/components`}>Components</Link>
-          </li>
-          <li>
-            <Link to={`${match.url}/props-v-state`}>
-              Props v. State
-            </Link>
-          </li>
-        </ul>
-  
-        {/* The Topics page has its own <Switch> with more routes
-            that build on the /topics URL path. You can think of the
-            2nd <Route> here as an "index" page for all topics, or
-            the page that is shown when no topic is selected */}
-        <Switch>
-          <Route path={`${match.path}/:topicId`}>
-            <Topic />
-          </Route>
-          <Route path={match.path}>
-            <h3>Please select a topic.</h3>
-          </Route>
-        </Switch>
-      </div>
-    );
-  }
-  
-  function Topic() {
-    let { topicId } = useParams();
-    return <h3>Requested topic ID: {topicId}</h3>;
-  }
+}
 
-  class App extends React.Component {
+function About() {
+    return <h2>About</h2>;
+}
+
+function Topics() {
+    let match = useRouteMatch();
+
+    return (
+        <div>
+            <h2>Topics</h2>
+
+            <ul>
+                <li>
+                    <Link to={`${match.url}/components`}>Components</Link>
+                </li>
+                <li>
+                    <Link to={`${match.url}/props-v-state`}>
+                        Props v. State
+                    </Link>
+                </li>
+            </ul>
+
+            <Switch>
+                <Route path={`${match.path}/:topicId`}>
+                    <Topic />
+                </Route>
+                <Route path={match.path}>
+                    <h3>Please select a topic.</h3>
+                </Route>
+            </Switch>
+        </div>
+    );
+}
+
+function Topic() {
+    let location = useLocation(); //取得url嵌入的參數
+    console.log("useLocation",location)
+    let { topicId } = useParams();
+    const dispatch = useDispatch();
+    const handleClick = () => {
+        dispatch({
+            type: 'set',
+            name: topicId
+        });
+    };
+    let display = null
+    if(topicId){
+        display = (
+            <button onClick={handleClick}>set name</button>
+        )
+    }
+    return (
+        <div>
+            <h3>Requested topic ID: {topicId}</h3>
+            {display}
+        </div>
+    )
+}
+
+class App extends React.Component {
     handleClick=()=>{
         const { dispatch } = this.props
         dispatch({
@@ -80,31 +97,36 @@ import {
                 <input type="text" onChange={this.handleChange} value={this.props.user}/>
                 <button onClick={this.handleClick}>Click me</button>
                 <Router>
-                <div>
-                    <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/about">About</Link>
-                    </li>
-                    <li>
-                        <Link to="/topics">Topics</Link>
-                    </li>
-                    </ul>
+                    <div>
+                        <ul>
+                            <li>
+                                <Link to={{
+                                    pathname: "/",
+                                    search: "?sort=name",
+                                    hash: "#the-hash",
+                                    state: { fromDashboard: true }
+                                }}>Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/about">About</Link>
+                            </li>
+                            <li>
+                                <Link to="/topics">Topics</Link>
+                            </li>
+                        </ul>
 
-                    <Switch>
-                    <Route path="/about">
-                        <About />
-                    </Route>
-                    <Route path="/topics">
-                        <Topics />
-                    </Route>
-                    <Route path="/">
-                        <Home />
-                    </Route>
-                    </Switch>
-                </div>
+                        <Switch>
+                            <Route path="/about">
+                                <About />
+                            </Route>
+                            <Route path="/topics">
+                                <Topics />
+                            </Route>
+                            <Route path="/">
+                                <Home />
+                            </Route>
+                        </Switch>
+                    </div>
                 </Router>
             </div>
         );
